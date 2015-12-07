dfcc0c9e59aac28ca08fbbbbe98ba3692006f5c9
AvneeshSarwate <avneeshsarwate@gmail.com>
2015-12-07 02:50:35 -0500

    figured out stateful dependent computation w partially applied functions

--- pydaltest.py
+++ pydaltest.py
@@ -1,5 +1,92 @@
 import PydalChanel as pydal
 
-ch1 = pydal.newChannel(1)
-ch1.play(pydal.read("sn bd bd"))
+read = pydal.read
 
+ch1 = pydal.newChannel(1)
+ch1.play(read("sn bd bd"))
+
+
+# TODO: probably want this implementation 
+# To implement a functor, a user must override the 
+# initializeState and render methods 
+class Functor:
+
+	def __init__(self, *args):
+		self.args = args
+		self.initializeState()
+	
+	def initializeState(self):
+		return
+
+	# A functor can be called repeatedly 
+	# to accumulate all needed arguments.
+	# Its arguments must be called in order.
+	# (todo: support explicit currying a la supercollider?)
+	def __call__(self, *args):
+		self.args += args
+
+	# Render returns a sample time series and  
+	# only references its own state and saved arguments.
+	# It may modify its own state variables.
+	# If an expression tree must be returned, it should be set here
+	def render(self):
+		return
+
+	# If a functor makes changes to the expression tree
+	# this is the method that should be used to get it. 
+	def getExpressionTree():
+		return self.expressionTree
+
+
+
+
+
+# To implement a functor, a user must pass in 2 functions
+# initializer - takes "self" as first argument and 
+# 	sets self.[name] to initial state values as specified by args
+# renderer - takes "self" as first argument and does exactly what is 
+# 	described in the comment for the render method of FunctorBuilder
+class FunctorBuilder:
+
+	def __init__(self, initializer, renderer):
+		self.initializer = initializer
+		self.renderer = renderer
+		
+	
+	def initializeState(self):
+		self.initilizer(self)
+
+	# A functor can be called repeatedly 
+	# to accumulate all needed arguments.
+	# Its arguments must be called in order.
+	# (todo: support explicit currying a la supercollider?)
+	def __call__(self, *args):
+		self.args += args
+
+	# Render returns a sample time series and  
+	# only references its own state and saved arguments.
+	# It may modify its own state variables.
+	# If an expression tree must be returned, it should be set here
+	def render(self):
+		self.renderer(self, *self.args)
+
+	# If a functor makes changes to the expression tree
+	# this is the method that should be used to get it. 
+	def getExpressionTree():
+		return self.expressionTree
+
+
+class EveryBy:
+
+	# args are int, float, pattern
+	def __init__(self, *args):
+		self.args = args
+
+	def render(self):
+		#some stuff
+		return 
+
+
+class Functor:
+
+	def __init__(self):
