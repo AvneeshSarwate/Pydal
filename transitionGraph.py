975135203e6e7945f87bf631136753a9e7e45892
AvneeshSarwate <avneeshsarwate@gmail.com>
2016-03-07 01:38:03 -0500

    simple testing of graph logic, still no OSC stuff implemented

--- transitionGraph.py
+++ transitionGraph.py
@@ -2,13 +2,6 @@ import re
 import itertools
 
 
-gStr = """
-f1 --(s1)--> f2 --(pad1)--> f4
-
-f2 --(seq1)--> f3
-
-"""
-
 def funcStateTest(f, *args):
 	print "function", f, "with args", args
 
@@ -27,14 +20,17 @@ def main():
 
 	tg = TransitionGraph(funcMap, gStr)
 
+	print tg.toGraphVisString()
+
+	print tg.states
+	tg.transitionHandler("s1", 0, 0, 0)
+	print tg.states
+
 # general idea - a "state" consists of a functor that 
 # is called when that state is transitioned to, its arguments being those that
 # are provided from the "transition message" that signals the move to that state
 
 # a "transition" is an OSC message with certain arguments
-
-
-
 class TransitionGraph:
 
 	def __init__(self, stateKeyToFunctor, graphString, initialStates = None):
@@ -43,7 +39,7 @@ class TransitionGraph:
 		self.graphString = graphString
 		self.tokens = self.parseGraphStringIntoTokens()
 		#list of current active state strings (first state in graph string by default)
-		self.states = Set(self.tokens[0][0]) if initialStates is None else initialStates 
+		self.states = set([self.tokens[0][0]]) if initialStates is None else initialStates 
 		self.generateGraphFromTokens(self.tokens)
 
 		#todo - instantiate OSC server
@@ -60,48 +56,55 @@ class TransitionGraph:
 			lineTokens = filter(lambda a: len(a) != 0, line.split())
 			i = 0
 			m = lambda reStr, argStr: re.match(reStr, argStr)
-			while i < len(lineTokens):
+
+			#move forward 2 tokens each time
+			while i < len(lineTokens)-1:
 				if not (m(symbolRE, lineTokens[i+0]) and m(arrowRE, lineTokens[i+1]) and m(symbolRE, lineTokens[i+2])):
 					raise StopIteration("improperly formatted graph string")
 				i += 2
 
 			tokens.append(lineTokens)
 
-
+		for t in tokens:
+			print t
 		return tokens
 
 
 	def generateGraphFromTokens(self, tokens):
 		for lineTokens in tokens:
+			print "LINE TOKENS", lineTokens
 			i = 0
-			while i < len(lineTokens):
-				transitionStr = lineTokens[1][3:-4]
+
+			#move forward 2 tokens each time
+			while i < len(lineTokens)-1:
+				transitionStr = lineTokens[i+1][3:-4]
 				if not transitionStr in self.linksByTransition:
-					linksByTransition[transitionStr] = Set()
-				linksByTransition[transitionStr].add(Link(lineTokens[0], transitionStr, lineTokens[2]))	
+					self.linksByTransition[transitionStr] = set()
+				self.linksByTransition[transitionStr].add(Link(lineTokens[i+0], transitionStr, lineTokens[i+2]))	
+				print i, lineTokens[i+0], transitionStr, lineTokens[i+2]
 				#todo: add osc hanlder (self.transitionHanlder)
 				i += 2
 
 
 	def transitionHandler(self, addr, tags, stuff, source):
-		linksFromOldStates = filter(lambda link: link.fromState in states, linksByTransition[addr])
+		linksFromOldStates = filter(lambda link: link.fromState in self.states, self.linksByTransition[addr])
 		newStates = map(lambda link:  link.toState, linksFromOldStates)
 		
 		for state in newStates:
-			stateKeyToFunctor[state](*stuff)
+			self.stateKeyToFunctor[state](stuff)
 
-		self.states = newStates
+		self.states = set(newStates)
 
-	def toGraphVisString():
+	def toGraphVisString(self):
 		dotFileLines = []
-		for link in self.linksByTransition.values():
-			dotFileLines.append(link.fromState + " --> " + "("+link.transition+")" + "-->" + link.toState)
+		for link in itertools.chain.from_iterable(self.linksByTransition.values()):
+			dotFileLines.append(link.fromState + " -> " + "\"("+link.transition+")\"" + " -> " + link.toState)
 
 		return "digraph G {\n" + "\n".join(dotFileLines) + "\n}"
 
 class Link:
 
-	def __init__(fromStateString, transString, toStateString):
+	def __init__(self, fromStateString, transString, toStateString):
 		self.fromState = fromStateString
 		self.transition = transString
 		self.toState = toStateString
